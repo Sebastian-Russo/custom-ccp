462b61bc9e25f91952e35a22384d5deb8bf0a4a3
Sebastian Russo <russo.sebastian@gmail.com>
2024-06-02 07:03:37 -0600

    Initiated basic app.

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+import 'app.css';
+
+function App() {
+    return (
+        <div className='App'>
+            <header className=''>
+                <img scr={logo} className='app-logo' alt="logo" />
+                <p>
+                    Custom CCP
+                </p>
+            </header>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as ReactDOM from 'react-dom';
+import { App } from './components/app';
+
+
+//--- App Configuration ---\\
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
